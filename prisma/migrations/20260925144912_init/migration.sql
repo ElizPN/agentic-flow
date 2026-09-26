846ec77b5491b1ff1bc88f028932fe299fa9ac40
ElizPN <pankevych.liza@gmail.com>
2026-09-26 12:37:49 +0200

    feat: add initial migration for User model and user_role enum

---
+++ prisma/migrations/20260925144912_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "user_role" AS ENUM ('PARTICIPANT', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "role" "user_role" NOT NULL DEFAULT 'PARTICIPANT',
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
